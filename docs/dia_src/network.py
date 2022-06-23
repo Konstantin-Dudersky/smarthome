693e1d3aec303aa28894911d16c937da6d5d2922
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2022-06-23 08:50:56 +0300

    update diagrams

--- docs/dia_src/network.py
+++ docs/dia_src/network.py
@@ -15,18 +15,27 @@ tv = Node(name="TV")
 rasp = Node(name="Raspberry")
 
 
-net1 = Network(address="192.168.100.x")
-net1.add_node(terminal, ["192.168.100.1"])
-net1.add_node(router, ["192.168.100.2"])
+net = Network(address="192.168.100.x")
+net.add_node(terminal, ["192.168.100.1"])
+net.add_node(router, ["192.168.100.2"])
 
-net2 = Network(address="192.168.101.x")
-net2.add_node(router, ["192.168.101.1"])
-net2.add_node(rasp, ["192.168.101.10", "192.168.101.11"])
-net2.add_node(pc, ["192.168.101.11", "192.168.101.13"])
-net2.add_node(tv, ["192.168.101.14", "192.168.101.15"])
+net_eth = Network(address="192.168.101.x", name="Ethernet")
+net_eth.add_node(router, ["192.168.101.1"])
+net_eth.add_node(rasp, ["192.168.101.10"])
+net_eth.add_node(pc, ["192.168.101.12"])
+net_eth.add_node(tv, ["192.168.101.14"])
+
+net_wifi = Network(address="192.168.101.x", name="WiFi")
+net_wifi.add_node(router, ["192.168.101.1"])
+net_wifi.add_node(rasp, ["192.168.101.11"])
+net_wifi.add_node(pc, ["192.168.101.13"])
+net_wifi.add_node(tv, ["192.168.101.15"])
+
+net_zigbee = Network(name="Zigbee")
+net_zigbee.add_node(rasp)
 
 dia = Diagram(
     filename='network',
-    networks=[net1, net2],
+    networks=[net, net_eth, net_wifi, net_zigbee],
     peer_networks=[PeerNetwork(inet, terminal)]
 )
